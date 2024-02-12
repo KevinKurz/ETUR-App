fb2b617336253222993931505efec5bca822c0d2
John Doe <orburzul@gmail.com>
2024-02-12 15:56:22 +0100

    json erstellt

---
+++ src/services/customer-number-server/customers.js
@@ -0,0 +1,42 @@
+const user = [
+  {
+    customerId: "001",
+    customerName: "Kevin",
+    customerTel: "01223456",
+    customerEmail: "kevin@gmail.com",
+  },
+  {
+    customerId: "002",
+    customerName: "Sinan",
+    customerTel: "01223456",
+    customerEmail: "Sinan@gmail.com",
+  },
+  {
+    customerId: "003",
+    customerName: "Max",
+    customerTel: "01223456",
+    customerEmail: "kevin@gmail.com",
+  },
+  {
+    customerId: "004",
+    customerName: "Sara",
+    customerTel: "01223456",
+    customerEmail: "Sara@gmail.com",
+  },
+  {
+    customerId: "005",
+    customerName: "Markus",
+    customerTel: "01223456",
+    customerEmail: "Markus@gmail.com",
+  },
+  {
+    customerId: "006",
+    customerName: "Niklas",
+    customerTel: "01223456",
+    customerEmail: "Niklas@gmail.com",
+  },
+];
+
+
+
+
