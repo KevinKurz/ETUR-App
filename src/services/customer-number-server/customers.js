1c20578160eadb547f3610ebf52ee0a308820091
John Doe <orburzul@gmail.com>
2024-02-13 16:01:07 +0100

    request methods created

--- src/services/customer-number-server/customers.js
+++ src/services/customer-number-server/customers.js
@@ -37,28 +37,6 @@ const customers = [
   },
 ];
 
-export function getAllCustomers() {
-  return customers;
-}
-
-export function getCustomerWidthID(id) {
-  for (let i = 0; i < customers.length; i++) {
-    if (customers[i].customerId === id) {
-      return customers[i];
-    }
-  }
-}
-
-export function deleteCustomerWidthID(id) {
-  for (let i = 0; i < customers.length; i++) {
-    if (customers[i].customerId === id) {
-      customers.splice(i, 1);
-      break;
-    }
-  }
-  console.log(customers);
-}
-
 export function validateCustomerNumber(customerNumber) {
   const pattern = /^ETUR-CN-\d+$/;
   const isValid = pattern.test(customerNumber);
@@ -77,12 +55,50 @@ export function validateCustomerNumber(customerNumber) {
   return false;
 }
 
+const customerSchema = {
+  schema: {
+    body: {
+      type: "object",
+      properties: {
+        customerId: { type: "string" },
+        customerName: { type: "string" },
+        customerTel: { type: "string" },
+        customerEmail: { type: "string" },
+      },
+      required: ["customerId", "customerName", "customerEmail"],
+    },
+  },
+};
+
+const opts = {
+  schema: {
+    response: {
+      200: {
+        type: "array",
+        items: {
+          type: "object",
+          properties: {
+            customerId: { type: "string" },
+            customerName: { type: "string" },
+            customerTel: { type: "string" },
+            customerEmail: { type: "string" },
+          },
+        },
+      },
+    },
+  },
+};
+
+function getCustomerWithID(id) {
+  return customers.find((customer) => customer.customerId === id);
+}
+
 export async function routes(fastify, options) {
   fastify.get("/customers", async (request, reply) => {
     return customers;
   });
 
-  fastify.get("/customers/:id", async (request, reply) => {
+  fastify.get("/customer/:id", async (request, reply) => {
     const customerId = request.params.id;
     const customer = getCustomerWithID(customerId);
     if (customer) {
@@ -92,7 +108,7 @@ export async function routes(fastify, options) {
     }
   });
 
-  fastify.post("/customers", async (request, reply) => {
+  fastify.post("/customer", async (request, reply) => {
     const newCustomerData = request.body;
     if (
       !newCustomerData.customerId ||
@@ -119,7 +135,7 @@ export async function routes(fastify, options) {
     }
   });
 
-  fastify.delete("/customers/:id", async (request, reply) => {
+  fastify.delete("/customer/:id", async (request, reply) => {
     const customerId = request.params.id;
     const isDeleted = deleteCustomerWithID(customerId);
     if (isDeleted) {
@@ -130,8 +146,49 @@ export async function routes(fastify, options) {
   });
 }
 
-export function createNewCustomer(newCustomer)
-{
-  customers.push(newCustomer)
-  console.log(customers)
+export function getAllCustomers() {
+  const response = fetch("http://localhost:3000/customers", {
+    mode: "no-cors",
+    method: "Get",
+  });
+  const customers = response.json();
+  return customers;
+}
+
+export async function getCustomerWidthID(id) {
+  const response = await fetch(`http://localhost:3000/customer/${id}`, {
+    mode: "no-cors",
+    method: "Get",
+  });
+  const customer = await response.json();
+  return customer;
+}
+
+export async function deleteCustomerWidthID(id) {
+  const response = await fetch(`http://localhost:3000/customer/${id}`, {
+    mode: "no-cors",
+    method: "DELETE",
+  });
+  return response.ok;
+}
+
+export async function postNewCustomer(newCustomer) {
+  try {
+    const response = await fetch("http://localhost:3000/customer", {
+      mode: "no-cors",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newCustomer),
+    });
+
+    if (!response.ok) {
+      throw new Error("Something went wrong");
+    }
+    const result = await response.json();
+    console.log(result);
+  } catch (error) {
+    console.error(error);
+  }
 }
